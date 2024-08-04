6197159fdcd519ef126196ff5beb9125b8aeba12
your_username <benbrill101@gmail.com>
2024-08-03 21:55:24 -0700

    update ports and connection

--- app/config.py
+++ app/config.py
@@ -1,7 +1,8 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://admin:D0ntWasteClothes@fit-check-1.cha4ewuauoz3.us-east-1.rds.amazonaws.com/fitcheck'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:D0ntWasteClothes@fit-check-1.cha4ewuauoz3.us-east-1.rds.amazonaws.com/fitcheck'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'fit-check', 'public', 'images')
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
+    
